3e1e39dcc4ede02e3508a44a8b204c9800444de0
LeviiValenok <gaydinapolia@gmail.com>
2023-02-22 14:23:42 +0300

    setup project

--- src/layouts/main/mainLayout.tsx
+++
@@ -1,5 +0,0 @@
-import { MainRouter } from 'routers';
-
-export function MainLayout() {
-  return <MainRouter />;
-}
--- src/pages/signIn/signInPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function SignInPage() {
-  return <div>signInPage</div>;
-}
